041442dbca33308c10d61129c27c176d14aceaae
Note Patiparn <notexpen@gmail.com>
2021-07-25 17:41:16 +0700

    first commit

---
+++ JsDay#1/index.js
@@ -0,0 +1,17 @@
+function playingDrumKit(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio) return; // return the keyboard event that don't have auio right away so it no need to be run in this function 
+    audio.currentTime = 0; // to rewind to the start 
+    audio.play()
+    key.classList.add('playing')
+}
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return 
+    this.classList.remove('playing');
+}
+ 
+const keys = document.querySelectorAll('.key') 
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playingDrumKit)
